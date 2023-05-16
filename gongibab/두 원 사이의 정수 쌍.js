bec3d52f734389b52a0d6a69a7d4e3546b2419d4
Habyte <qwaszxed1234@gmail.com>
2023-05-16 17:54:58 +0900

    [PGS] 두 원 사이의 정수 쌍 / 181187 / 레벨2 / 수정

--- gongibab/두 원 사이의 정수 쌍.js
+++ gongibab/두 원 사이의 정수 쌍.js
@@ -1,3 +1,10 @@
+/**
+ * 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181187
+ * 문제 유형: 구현
+ * 풀이 시간: 60분
+ * 성공 여부: 아니요(60%)
+ */
+
 function solution(r1, r2) {
   let answer = 0;
   for (let i = r2; i >= 0; i--) {
