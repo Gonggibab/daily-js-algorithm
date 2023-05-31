586c6b97748246781e9b93187caeeacd5a254180
Habyte <qwaszxed1234@gmail.com>
2023-05-31 20:16:02 +0900

    [PGS] 요격시스템 / 181188 / 레벨2 / 두번째 풀이

--- gongibab/호텔 대실.js
+++ gongibab/호텔 대실.js
@@ -3,6 +3,10 @@
  * 문제 유형: 정렬, 구현
  * 풀이 시간: 45분
  * 성공 여부: 아니요 (15.8%)
+ *
+ * 두번째 풀이
+ * 풀이 시간: 25분
+ * 성공 여부: 예
  */
 
 function solution(book_time) {
@@ -11,28 +15,25 @@ function solution(book_time) {
     let [start, end] = time;
     start = start.split(":");
     end = end.split(":");
-    times.push([
-      start[0] * 60 + Number(start[1]),
-      end[0] * 60 + Number(end[1]) + 10,
-    ]);
+    times.push([start[0] * 60 + Number(start[1]), "in"]);
+    times.push([end[0] * 60 + Number(end[1]) + 10, "out"]);
   });
-  times.sort((a, b) => a[0] - b[0]);
+
+  times.sort((a, b) => {
+    if (a[0] === b[0]) {
+      if (a[1] !== b[1] && b[1] === "out") return 1;
+      else return -1;
+    } else return a[0] - b[0];
+  });
 
   let answer = 0,
-    count = 1,
-    start = 0;
-  end = 0;
+    count = 0;
   times.forEach((time) => {
-    let [s, e] = time;
-    if (s < end) {
-      end = Math.min(end, e);
-      count += 1;
-    } else {
-      answer = Math.max(answer, count);
-      end = e;
-      count = 1;
-    }
+    let [_, type] = time;
+    if (type === "in") count += 1;
+    else count -= 1;
+    answer = Math.max(answer, count);
   });
 
-  return Math.max(answer, count);
+  return answer;
 }
